632cda3659c49c19a4d03a25c04692af7fe495e0
adarsh-jain-a <adarshjain9628@gmail.com>
2025-10-21 18:49:45 +0530

    company setings

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,5 +1,7 @@
+import * as React from "react";
 import {useState} from "react";
 import {cn} from "@/lib/utils.ts";
+import {Input} from "@/components/ui/input.tsx";
 
 export interface IPropsDatePicker {
     onInput?: (date: string) => void;
@@ -9,21 +11,29 @@ export interface IPropsDatePicker {
     className?: string;
 }
 
-export default function DatePicker(props: IPropsDatePicker) {
-    const [isError, setIsError] = useState<boolean>(false);
-    return <input
+export default function DatePicker({
+   onInput,
+   onEnter,
+   defaultValue,
+   isError,
+   className,
+   ...props
+}: IPropsDatePicker & React.ComponentProps<"input">) {
+    const [isInternalError, setIsError] = useState<boolean>(false);
+    return <Input
+        {...props}
         type="date"
-        defaultValue={props.defaultValue}
+        defaultValue={defaultValue}
         onInput={(e) => {
             setIsError(!e.currentTarget.value)
-            props.onInput?.(e.currentTarget.value)
+            onInput?.(e.currentTarget.value)
         }}
         onKeyDown={(event) => {
             if (event.key === "Enter") {
                 const target = event.target as HTMLInputElement;
-                props.onEnter?.(target.value);
+                onEnter?.(target.value);
             }
         }}
-        className={cn(`selection:bg-primary selection:text-primary-foreground ${isError || props.isError ? 'border-destructive focus-visible:border-destructive focus-visible:ring-destructive/50 focus-visible:ring-[3px]' : 'border-input focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]'} h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm aria-invalid:ring-destructive/20 aria-invalid:border-destructive [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none`, props.className)}
+        className={cn(`selection:bg-primary selection:text-primary-foreground ${isInternalError || isError ? 'border-destructive focus-visible:border-destructive focus-visible:ring-destructive/50 focus-visible:ring-[3px]' : 'border-input focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]'} h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm aria-invalid:ring-destructive/20 aria-invalid:border-destructive [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none`, className)}
     />;
 }
