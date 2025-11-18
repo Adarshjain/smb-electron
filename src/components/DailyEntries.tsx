c2f907a9d054bf183b2e83c89bf6b40d4e3ad0e2
adarsh-jain-a <ajain@bolt.com>
2025-11-18 20:45:43 +0530

    calculate closing balance

--- src/components/DailyEntries.tsx
+++ src/components/DailyEntries.tsx
@@ -293,7 +293,6 @@ export default function DailyEntries() {
           navigated
         />
       </div>
-
       <DailyEntriesTables
         accountHeads={accountHeads}
         openingBalance={openingBalance}
