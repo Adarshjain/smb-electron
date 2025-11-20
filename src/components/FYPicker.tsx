8649ccc13eda4cd53f4488d9e8d23d8e6f85a6a9
adarsh-jain-a <adarshjain9628@gmail.com>
2025-11-20 23:26:17 +0530

    P&L fixes, fy picker

---
+++ src/components/FYPicker.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import {
+  NativeSelect,
+  NativeSelectOption,
+} from '@/components/ui/native-select';
+import { getFinancialYearRange } from '@/lib/myUtils.tsx';
+
+const years: number[] = [];
+for (let i = 2020; i < new Date().getFullYear() + 1; i++) {
+  years.push(i);
+}
+
+export default function FYPicker(props: {
+  onChange?: (date: [string, string]) => void;
+}) {
+  const [year, setYear] = useState<number>(new Date().getFullYear());
+
+  useEffect(() => {
+    props.onChange?.(getFinancialYearRange(year));
+  }, [year, props]);
+
+  return (
+    <NativeSelect
+      value={year}
+      onChange={(e) => setYear(parseInt(e.target.value))}
+    >
+      {years.map((year) => (
+        <NativeSelectOption key={year} value={year}>
+          FY {year}-{year + 1}
+        </NativeSelectOption>
+      ))}
+    </NativeSelect>
+  );
+}
